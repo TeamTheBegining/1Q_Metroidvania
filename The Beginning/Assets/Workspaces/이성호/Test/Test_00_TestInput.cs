58588b7cf0f216e8b07ab2fc2d32d3e393c45542
mob954325 <lsh954325@gmail.com>
2025-05-12 12:36:42 +0900

    테스트 코드 업로드 및 폴더 분류

---
+++ The Beginning/Assets/Workspaces/이성호/Test/Test_00_TestInput.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test_00_TestInput : TestBase
+{
+    protected override void OnTest1(InputAction.CallbackContext context)
+    {
+        Debug.Log("Test 1");    
+    }
+
+    protected override void OnTest2(InputAction.CallbackContext context)
+    {
+        Debug.Log("Test 2");
+    }
+
+    protected override void OnTest3(InputAction.CallbackContext context)
+    {
+        Debug.Log("Test 3");
+    }
+
+    protected override void OnTest4(InputAction.CallbackContext context)
+    {
+        Debug.Log("Test 4");
+    }
+
+    protected override void OnTest5(InputAction.CallbackContext context)
+    {
+        Debug.Log("Test 5");
+    }
+}
