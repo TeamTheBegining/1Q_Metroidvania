a054d4b71ddfc5bb0a791c6a7c9d9d28def8d3ea
koplm118 <67418726+koplm118@users.noreply.github.com>
2025-05-13 20:34:04 +0900

    인터페이스 수정

--- The Beginning/Assets/Shared/Interface/IDamageable.cs
+++ The Beginning/Assets/Shared/Interface/IDamageable.cs
@@ -16,12 +16,12 @@ public interface IDamageable
     /// <summary>
     /// 피격 가능한 오브젝트가 피격 시 실행되는 함수
     /// </summary>
-    public Action OnHit { get; set; }
+    //public Action OnHit { get; set; }
 
     /// <summary>
     /// 피격 가능한 오브젝트가 사망 시 실행되는 함수
     /// </summary>
-    public Action OnDead { get; set; }
+    //public Action OnDead { get; set; }
 
     /// <summary>
     /// 사망 확인 변수
