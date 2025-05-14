c55de2760541779c84649b0070254cdec00dc03a
mob954325 <lsh954325@gmail.com>
2025-05-14 18:05:06 +0900

    아이템 데이터 추가 완료

---
+++ The Beginning/Assets/Workspaces/이성호/Scripts/Data/ItemDataSO.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item_99", menuName = "ScriptableObject/ItemData", order = 1)]
+public class ItemDataSO : ScriptableObject
+{
+    public Sprite icon;
+
+    /// <summary>
+    /// 아이템 이름
+    /// </summary>
+    public string itemName;
+
+    /// <summary>
+    /// 아이템 설명
+    /// </summary>
+    [TextArea]
+    public string itemDescription;
+}
---
+++ The Beginning/Assets/Workspaces/이성호/Scripts/Editor/ItemDataCustomEditor.cs
@@ -0,0 +1,44 @@
+﻿ using UnityEngine;
+ using UnityEditor;
+
+[CustomEditor(typeof(ItemDataSO))]
+public class ItemDataCustomEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        ItemDataSO item = (ItemDataSO)target;
+
+        if(item.icon != null)
+        {
+            GUILayout.Label("아이콘 미리보기", EditorStyles.boldLabel);
+
+            // 이미지 정렬
+            float width = EditorGUIUtility.currentViewWidth - 40;
+            float size = Mathf.Min(128, width);
+
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+
+            Rect rect = GUILayoutUtility.GetRect(size, size);
+
+            Sprite sprite = item.icon;
+            Texture2D tex = sprite.texture;
+
+            Rect spriteRect = sprite.rect;
+            Rect uv = new Rect(
+                spriteRect.x / tex.width,
+                spriteRect.y / tex.height,
+                spriteRect.width / tex.width,
+                spriteRect.height / tex.height
+            );
+
+            GUI.DrawTextureWithTexCoords(rect, tex, uv);
+
+
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+        }
+    }
+}
