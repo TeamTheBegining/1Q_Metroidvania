d147263d1177860f7c29e529a0a8e5bd9f990cc6
KRBOB-0 <nbvjd1@gmail.com>
2025-05-14 16:44:59 +0900

    Update PlayerHpUI.cs
    maybe last? 더 건드릴 것 없을 듯

---
+++ The Beginning/Assets/Workspaces/박민성/UI/PlayerHpUI.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using static Player;
+
+public class PlayerHpUI : MonoBehaviour
+{
+    // Inspector에서 연결할 Slider
+    public Slider hpSlider;
+    public Slider mpSlider;
+
+    // PlayerStats 타입 스크립트에서 체력 값 가져옴
+    public Player player;
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        hpSlider.value = 1;
+        mpSlider.value = 0;
+
+        if (player == null) Debug.LogWarning($"{gameObject.name} 에서 플레이어를 찾을 수 없음 ");
+    }
+    void LateUpdate()
+    {
+        // 현재 체력 / 최대 체력 비율을 슬라이더에 반영
+        if(player != null)
+        {
+            hpSlider.value = (float)player.CurrentHp / player.MaxHp;
+            mpSlider.value = (float)player.CurrentMp / player.MaxMp;
+        }
+    }
+}
