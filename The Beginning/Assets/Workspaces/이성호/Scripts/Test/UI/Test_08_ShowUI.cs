8849ce9d517d8cf66e8fd95287790eff9a6d957a
mob954325 <lsh954325@gmail.com>
2025-05-19 18:42:22 +0900

    월드 가이드 텍스트 오브젝트 추가

---
+++ The Beginning/Assets/Workspaces/이성호/Scripts/Test/UI/Test_08_ShowUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test_08_ShowUI : TestBase
+{
+    public WorldText text;
+
+    protected override void OnTest1(InputAction.CallbackContext context)
+    {
+        text.ShowText();
+    }
+    protected override void OnTest2(InputAction.CallbackContext context)
+    {
+        text.HideText();
+    }
+    protected override void OnTest3(InputAction.CallbackContext context)
+    {
+        text.FadeInText();
+    }
+    protected override void OnTest4(InputAction.CallbackContext context)
+    {
+        text.FadeOutText();
+    }
+}
---
+++ The Beginning/Assets/Workspaces/이성호/Scripts/WorldObject/WorldText.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework.Interfaces;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class WorldText : MonoBehaviour
+{
+    private TextMeshPro textMeshPro;
+    public TextDataSO data;
+    public float duration;
+
+    private void Awake()
+    {
+        textMeshPro = GetComponentInChildren<TextMeshPro>();
+        textMeshPro.text = data.text;
+    }
+
+    public void ShowText()
+    {
+        textMeshPro.color = new Color(1, 1, 1, 1);
+    }
+
+    public void HideText()
+    {
+        textMeshPro.color = new Color(1, 1, 1, 0);
+    }
+
+    public void FadeInText()
+    {
+        StartCoroutine(FadeInProcess());
+    }
+
+    public void FadeOutText()
+    {
+        StartCoroutine(FadeOutProcess());
+    }
+
+    private IEnumerator FadeInProcess()
+    {
+        float timeElapsed = 0.0f;
+        float duration = 2f;
+
+        while (timeElapsed < duration)
+        {
+            timeElapsed += Time.deltaTime;
+            textMeshPro.color = new Color(1, 1, 1, timeElapsed / duration);
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeOutProcess()
+    {
+        float timeElapsed = 0.0f;
+        float duration = 1f;
+
+        while (timeElapsed < duration)
+        {
+            timeElapsed += Time.deltaTime;
+            textMeshPro.color = new Color(1, 1, 1, 1 - timeElapsed / duration);
+            yield return null;
+        }
+    }
+}
