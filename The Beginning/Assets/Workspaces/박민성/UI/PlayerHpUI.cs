1c0fb51686b0f675443c456043e3836beef55cb1
koplm118 <67418726+koplm118@users.noreply.github.com>
2025-05-20 11:32:02 +0900

    HP UI 수정

--- The Beginning/Assets/Workspaces/박민성/UI/PlayerHpUI.cs
+++ The Beginning/Assets/Workspaces/박민성/UI/PlayerHpUI.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using static Player;
@@ -7,6 +8,7 @@ public class PlayerHpUI : MonoBehaviour
     // Inspector에서 연결할 Slider
     public Slider hpSlider;
     public Slider mpSlider;
+    public TextMeshProUGUI parryingCountText;
 
     // PlayerStats 타입 스크립트에서 체력 값 가져옴
     public Player player;
@@ -15,6 +17,7 @@ public class PlayerHpUI : MonoBehaviour
         player = GameObject.FindWithTag("Player").GetComponent<Player>();
         hpSlider.value = 1;
         mpSlider.value = 0;
+        parryingCountText.text = "";
 
         if (player == null) Debug.LogWarning($"{gameObject.name} 에서 플레이어를 찾을 수 없음 ");
     }
@@ -25,6 +28,8 @@ public class PlayerHpUI : MonoBehaviour
         {
             hpSlider.value = (float)player.CurrentHp / player.MaxHp;
             mpSlider.value = (float)player.CurrentMp / player.MaxMp;
+            float count = player.CurParryCount > 3 ? 3 : player.CurParryCount;
+            parryingCountText.text = player.CurParryCount == 0 ? "" : "X" + count;
         }
     }
 }
